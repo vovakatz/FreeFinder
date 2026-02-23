80a0b1e58b811a9514e323655f747dbd2607468e
Vova Katz <vovakats@yahoo.com>
2026-02-22 22:36:43 -0500

    Initial Commit

---
+++ FreeFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreeFinder
+//
+//  Created by Vladimir Katz on 2/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FreeFinder/FreeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeFinderApp.swift
+//  FreeFinder
+//
+//  Created by Vladimir Katz on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct FreeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
